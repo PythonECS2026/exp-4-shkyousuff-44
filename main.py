175098319d440d846a5fb4fd23ca0c2562ed81cb
shkyousuff-44 <shkyousuff@gmail.com>
2026-03-03 14:41:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,23 @@
 # Date:
 
 # Write your code here
+print("Simple Interest Calculator")
+
+print("Enter principal amount:")
+p = float(input().split()[-1])
+
+print("Enter rate of interest:")
+r = float(input().split()[-1])
+
+print("Enter time period (in years):")
+t = float(input().split()[-1])
+
+si = (p * r * t) / 100
+
+def fmt(x):
+    if x.is_integer():
+        return f"{x:.1f}"
+    else:
+        return f"{x:g}"
+
+print(f"Simple Interest: {fmt(si)}")
